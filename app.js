b5a3eb0f8bd9b003739764e7baccbfd6766a28b5
Muntasir Hassan <muntasirhassan17@gmail.com>
2024-09-24 17:48:52 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputField = document.querySelector("input");
+const textArea = document.querySelector("textarea");
+
+window.onload = () => {
+  inputField.value = "";
+  textArea.value = "";
+};
